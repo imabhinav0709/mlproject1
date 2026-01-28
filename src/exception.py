414e0ba2a45b9a0cc9362da185199de04439a428
imabhinav0709 <abhinavmaddipati20@gmail.com>
2026-01-28 16:30:17 +0530

    Exception and Logging

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+from logger import logging
+
+def error_message_detail(error, error_detail)->str:
+    _,_,exc_tb = error_detail.exc_info()
+    if exc_tb is not None:
+        filename = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+    else:
+        filename = "unknown"
+        line_number = 0
+    error_message = "Error occured in python script name [{0}] line number[{1}] error [{2}]".format(
+        filename, line_number, str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+    
